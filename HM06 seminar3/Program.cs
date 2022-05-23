692eef5fa8e8524f90f6228bc45d8e690583c854
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-23 22:05:42 +0500

    Add task06 HW Seminar3

---
+++ HM06 seminar3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Куб четного числа {num} равен {num*num*num}");
+}
+else
+{
+Console.WriteLine("Введите другое число");
+}
