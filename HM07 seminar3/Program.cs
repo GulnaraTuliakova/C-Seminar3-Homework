c6e27e5343763f2494a3af098a2f37db54f734f3
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-23 22:16:50 +0500

    Add task07 HW seminar3

---
+++ HM07 seminar3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(0,2);
+    }
+}
+void PrintArray (int[] coll)
+{
+    int lengthColl = coll.Length;
+    for (int pos = 0; pos < lengthColl; pos++)
+    {
+      Console.Write(coll[pos] + " ");  
+    }
+}
+int [] array = new int[8];
+FillArray (array);
+PrintArray (array);
+Console.WriteLine();
+
