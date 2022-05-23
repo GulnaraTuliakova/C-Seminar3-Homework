fa0885e5bed4447aa8bde0fed6ba4237387bb80e
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-23 22:36:24 +0500

    Add task05 HM seminar3

---
+++ HM05 seminar3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Подсчитать сумму цифр в числе
+Console.WriteLine("Чтобы подсчитать сумму цифр в числе, введите число A");
+int number = int.Parse(Console.ReadLine()?? "");
+int sum = 0;
+while (number > 0)
+   {
+   sum = sum + number % 10;
+   number = number / 10;
+   }
+    Console.WriteLine (sum);
+
+   
+   
+   /* Console.WriteLine("Чтобы подсчитать сумму цифр в числе, введите число A");
+string number = Console.ReadLine()?? "";
+int length = number.Length;
+int sum = 0;
+for (int i = 0; i < length; i++)
+   {
+   sum = sum + number[i];
+    }
+    Console.WriteLine (sum);*/
