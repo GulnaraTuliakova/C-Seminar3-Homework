58c84953fd06eea01e0ca551e1aa7dc2f8d18566
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-23 20:23:06 +0500

    Add task02 HW seminar3

---
+++ HM02 seminar3/Program.cs
@@ -0,0 +1,8 @@
+﻿//Найти кубы чисел от 1 до N
+Console.WriteLine("Чтобы найти кубы чисел от 1 до N, введите число N");
+int n = int.Parse(Console.ReadLine()?? "");
+
+for (int num = 1; num <= n; num++)
+{
+    Console.WriteLine ($"{num} в кубе = {num * num * num}");
+}
