f62b597b3e91c5d80c3ed3414e565588a24fedac
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-23 20:29:21 +0500

    Add task03 HW seminar3

---
+++ HM03 seminar3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Найти сумму чисел от 1 до А
+
+Console.WriteLine("Чтобы найти сумму чисел от 1 до A, введите число A");
+int a = int.Parse(Console.ReadLine()?? "");
+int sum = 0;
+for (int num = 1; num <= a; num++)
+{
+   sum = sum + num;
+  }
+  Console.WriteLine ($" Сумма чисел от 1 до {a} равна {sum}");
