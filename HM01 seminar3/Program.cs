4bd42e188bbd83c2a1d9d653f1aa722f58b768a6
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-23 20:17:36 +0500

    Add task01 HW seminar3

---
+++ HM01 seminar3/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Чтобы показать таблицу квадратов чисел от 1 до N, введите число N");
+int n = int.Parse(Console.ReadLine()?? "");
+
+for (int num = 1; num <= n; num++)
+{
+    Console.WriteLine ($"{num} в квадрате равен {num * num}");
+}
